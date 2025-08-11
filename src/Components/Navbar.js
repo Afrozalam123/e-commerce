8fae70f2670762bc90cbf4967f9e8f8e2d3fd00f
Afrozalam123 <alamafroz70400@gmail.com>
2025-08-11 16:16:42 +0530

    updated new and old code and new file existing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Navbar.css'
 export default function Navbar() {
   return (
     <nav className="bg-amber-900">
-      <ul className="px-28 py-4 flex text-white justify-end space-x-11 cursor-pointer">
+      <ul className="px-28 py-5 flex text-white justify-end space-x-6 cursor-pointer">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/catalogue">Catalogue</Link></li>
